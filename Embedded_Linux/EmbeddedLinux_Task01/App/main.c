6bf9a4ac55d109967cf3155b9da9fcf697e1a9b9
AbdelazizMaher <abdelazizmaher17499@gmail.com>
2024-03-05 18:26:02 +0200

    update main

--- Embedded_Linux/EmbeddedLinux_Task01/App/main.c
+++ Embedded_Linux/EmbeddedLinux_Task01/App/main.c
@@ -9,15 +9,49 @@
 
 int main()
 {
-	double Val1 = 11;
+
+	double In1 , In2;
+	char Operation;
+
+	printf("Enter one of the following operation( + , - , * , / , %% ): ");
+	Operation = getchar();	
+		
+	printf("Enter the first argument: ");
+	scanf("%lf",&In1);
+	
+	printf("Enter the second argument: ");
+	scanf("%lf",&In2);
+		
+	switch(Operation)
+	{
+	case '+':
+		printf("%f + %f = %f\n", In1, In2 ,Add(In1 ,In2));
+		break;
+	case '-':
+		printf("%f - %f = %f\n", In1, In2 ,Subtract(In1 ,In2));
+		break;
+	case '*':
+		printf("%f * %f = %f\n", In1, In2 ,Multiply(In1 ,In2));
+		break;
+	case '/':
+		printf("%f / %f = %f\n", In1, In2 ,Divide(In1 ,In2));
+		break;
+	case '%':
+		printf("%f %% %f = %f\n", In1, In2 ,Remainder(In1 ,In2));
+		break;
+	default:
+		printf("wrong operation\n");									
+	}
+	
+	/*double Val1 = 11;
 	double Val2 = 13.4;
 	printf("\n===================Simple Calculator================\n");
 	printf("%f + %f = %f\n", Val1, Val2 ,Add(Val1 ,Val2));
 	printf("%f - %f = %f\n",  Val1, Val2 ,Subtract(Val1 ,Val2));
 	printf("%f * %f = %f\n",  Val1, Val2 ,Multiply(Val1 ,Val2));
 	printf("%f / %f = %f\n",  Val1, Val2 ,Divide(Val1 ,Val2));
-	printf("%f MOD %f = %f\n",  Val1, Val2 ,Remainder(Val1 ,Val2)); 
-	printf("====================================================\n");	
+	printf("%f %% %f = %f\n",  Val1, Val2 ,Remainder(Val1 ,Val2)); 
+	printf("====================================================\n");*/	
 	
 	return 0;
 }
