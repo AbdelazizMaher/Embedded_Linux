6b069f62fde562791e0636d4e61f6515fe980775
AbdelazizMaher <abdelazizmaher17499@gmail.com>
2024-03-05 09:18:27 +0200

    calc_App

--- Embedded_Linux/EmbeddedLinux_Task01/App/main.c
+++ Embedded_Linux/EmbeddedLinux_Task01/App/main.c
@@ -11,12 +11,14 @@ int main()
 {
 	double Val1 = 11;
 	double Val2 = 13.4;
-	
+	printf("\n===================Simple Calculator================");
 	printf("Val1 + Val2 = %f\n", Add(Val1 ,Val2));
 	printf("Val1 - Val2 = %f\n", Subtract(Val1 ,Val2));
 	printf("Val1 * Val2 = %f\n", Multiply(Val1 ,Val2));
 	printf("Val1 / Val2 = %f\n", Divide(Val1 ,Val2));
-	printf("Val1 % Val2 = %f\n", Remainder(Val1 ,Val2)); 
+	printf("Val1 MOD Val2 = %f\n", Remainder(Val1 ,Val2)); 
+	printf("====================================================\n");	
+	
 	return 0;
 }
 
