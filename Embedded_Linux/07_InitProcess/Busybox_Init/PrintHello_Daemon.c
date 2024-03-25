705f086a35a11a1e8ce9a5fc9e4a587378a69e73
AbdelazizMaher <abdelazizmaher17499@gmail.com>
2024-03-25 07:24:34 +0200

    busybox init

---
+++ Embedded_Linux/07_InitProcess/Busybox_Init/PrintHello_Daemon.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	
+	printf("[Hello]: BusyBox init....\n");
+	printf("[Hello]: BusyBox init....\n");
+	printf("[Hello]: BusyBox init....\n");
+	printf("[Hello]: BusyBox init....\n");
+	printf("[Hello]: BusyBox init....\n");
+		
+	return 0;
+}
